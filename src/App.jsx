c928894fdb18526f21976a2bcd6f1314700e05be
Sophie Crabtree <sophiejulianacrabtree@gmail.com>
2023-10-11 18:16:29 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Sophie Crabtree</h1>
+      <h2>Software Developer</h2>
+    </>
+  )
+}
+
+export default App
